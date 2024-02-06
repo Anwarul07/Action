86df4779e7d14a18342883ec9cd3d1de23ed03dc
Anwarul07 <mdanwarul064@gmail.com>
2024-02-06 21:16:48 +0530

    js update

--- action.js
+++ action.js
@@ -1,9 +1,13 @@
+let sound = new Audio("files/music.mp3")
+// sound.play();
 document.onkeydown = (e) => {
+
     if ((e.keyCode === 38) || (e.keyCode === 104)) {
-        console.log(" key used for up")
-    } else if ((e.keyCode === 40) || (e.keyCode === 98)) {
-        console.log("key used for down")
-    } else {
-        console.log("Error")
+        dianos = document.querySelector(".dino")
+        dianos.classList.add('dianomation')
+        setTimeout(() => {
+            dianos.classList.remove("dianomation")
+        }, 700)
+        console.log("cliked", e.keyCode)
     }
 }
