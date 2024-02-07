37dc167320d8f1e8cd17d72a1b348ff30ac81e5d
Anwarul07 <mdanwarul064@gmail.com>
2024-02-07 21:46:10 +0530

    update

--- action.js
+++ action.js
@@ -11,3 +11,23 @@ document.onkeydown = (e) => {
         console.log("cliked", e.keyCode)
     }
 }
+
+setInterval(() => {
+    dianos = document.querySelector(".dino")
+    gameOver = document.querySelector(".gameover")
+    obsatacle = document.querySelector(".obsatacle")
+    dx = parseInt(window.getComputedStyle(dianos, null).getPropertyValue("left"))
+    dy = parseInt(window.getComputedStyle(dianos, null).getPropertyValue("top"))
+
+    ox = parseInt(window.getComputedStyle(obsatacle, null).getPropertyValue("left"))
+    oy = parseInt(window.getComputedStyle(obsatacle, null).getPropertyValue("top"))
+    offsetx = Math.abs(dx - ox);
+    offsetY = Math.abs(dy - oy);
+
+    if ((offsetx < 200) && (offsetY < 80)) {
+        gameOver.style.visibility = "visible";
+        obsatacle.classList.remove("obstanomation");
+    }
+    console.log("checking....")
+
+}, 100);
